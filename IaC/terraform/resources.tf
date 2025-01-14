d8c23b97cba37452bb2e22d8e6cc780ed3f58216
ablil <ablil@proton.me>
2025-01-14 15:40:09 +0100

    feat: Integrate Pub/Sub for Partner Configuration Management
    This commit introduces a new Pub/Sub-based system for managing partner configurations.  It includes the creation of a Pub/Sub topic and subscription, a schema for partner configuration messages, a message producer script, and a consumer that persists the configurations to the database.  The Cloud Run service is updated to subscribe to the new subscription.  This improves the system's scalability and reliability.

--- IaC/terraform/resources.tf
+++ IaC/terraform/resources.tf
@@ -3,7 +3,7 @@ resource "google_artifact_registry_repository" "ghcr" {
   repository_id = "ghcr-${var.environment}"
   description   = "Docker images registy for ${var.environment}"
   format        = "DOCKER"
-  mode = "REMOTE_REPOSITORY"
+  mode          = "REMOTE_REPOSITORY"
 
   remote_repository_config {
     description = "Github container registry"
@@ -14,36 +14,96 @@ resource "google_artifact_registry_repository" "ghcr" {
 }
 
 resource "google_firestore_database" "database" {
-  project     = var.project_id
-  name        = "partnerworld-${var.environment}"
-  location_id = var.region
-  type        = "DATASTORE_MODE"
+  project         = var.project_id
+  name            = "partnerworld-${var.environment}"
+  location_id     = var.region
+  type            = "DATASTORE_MODE"
   deletion_policy = "DELETE"
 }
 
 
 resource "google_cloud_run_v2_service" "partnerworld" {
-  name     = "partnerworld-${var.environment}"
-  location = var.region
+  name                = "partnerworld-${var.environment}"
+  location            = var.region
   deletion_protection = false
-  ingress = "INGRESS_TRAFFIC_ALL"
+  ingress             = "INGRESS_TRAFFIC_ALL"
 
   template {
     containers {
       image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.ghcr.repository_id}/ablil/partnerworld:latest"
       env {
-          name = "PROJECT_ID"
-          value = var.project_id
+        name  = "PROJECT_ID"
+        value = var.project_id
       }
       env {
-          name = "DATABASE_ID"
-          value = google_firestore_database.database.name
+        name  = "DATABASE_ID"
+        value = google_firestore_database.database.name
       }
       env {
-          name = "SPRING_PROFILES_ACTIVE"
-          value = "cloud,${var.environment}"
+        name  = "SPRING_PROFILES_ACTIVE"
+        value = "cloud,${var.environment}"
+      }
+      env {
+        name  = "subscription"
+        value = google_pubsub_subscription.partner_pull_subscription.name
       }
     }
   }
-  depends_on = [google_artifact_registry_repository.ghcr, google_firestore_database.database]
+  depends_on = [
+    google_artifact_registry_repository.ghcr, google_firestore_database.database,
+    google_pubsub_subscription.partner_pull_subscription
+  ]
+}
+
+// pubsub related resources
+resource "google_pubsub_topic" "partner_configurations_topic" {
+  name                       = "partner-configurations"
+  message_retention_duration = "3600s"
+  schema_settings {
+    schema   = google_pubsub_schema.configuration_schema.id
+    encoding = "JSON"
+  }
+
+  depends_on = [google_pubsub_schema.configuration_schema]
+}
+
+resource "google_pubsub_schema" "configuration_schema" {
+  name       = "partnerconfiguration"
+  type       = "AVRO"
+  definition = <<EOF
+    {
+     "type" : "record",
+     "name" : "Avro",
+     "fields" : [
+       {
+         "name" : "shortName",
+         "type" : "string"
+       },
+       {
+         "name" : "displayName",
+         "type" : "string"
+       }
+     ]
+    }
+  EOF
+}
+
+resource "google_pubsub_subscription" "partner_pull_subscription" {
+  name  = "partner-configuration"
+  topic = google_pubsub_topic.partner_configurations_topic.id
+
+  message_retention_duration = "1200s" # 20 minutes
+  retain_acked_messages = true
+  ack_deadline_seconds  = 10
+
+  expiration_policy {
+    ttl = "604800s" # 7 days
+  }
+
+  retry_policy {
+    minimum_backoff = "10s"
+  }
+
+  enable_message_ordering = false
+  depends_on = [google_pubsub_topic.partner_configurations_topic]
 }
---
+++ src/main/kotlin/io/ablil/configuration/pubsub/PartnerConfigurationMessage.kt
@@ -0,0 +1,10 @@
+package io.ablil.configuration.pubsub
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
+
+data class PartnerConfigurationMessage @JsonCreator constructor(
+    @JsonProperty("displayName") val displayName: String,
+    @JsonProperty("shortName") val shortName: String
+)
